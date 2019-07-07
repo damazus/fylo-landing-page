b82f782c661eacc0794dc0f04c60bdb46b6a9992
damazus <liginmlugudor@gmail.com>
2019-07-07 20:24:45 +0000

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const { src, dest, series, watch } = require('gulp');
+const gulpSass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+function sass(){
+   return src("scss/*.scss", { sourcemaps: true })
+      .pipe(gulpSass().on('error', gulpSass.logError))
+      .pipe(dest("css", { sourcemaps: '.' }))
+      .pipe(browserSync.stream());
+}
+
+function serve(){
+   browserSync.init({
+      server: {
+         baseDir: "./"
+      }
+   });
+
+   watch("scss/**/*.scss", series(sass));
+   watch("*.html").on('change', browserSync.reload);
+   watch("js/**/*.js").on('change', browserSync.reload);
+}
+
+module.exports = {
+   default: serve
+}
---
+++ js/app.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const sr = ScrollReveal()
+    sr.reveal('.hero__picture, .hero__title, .hero__description, .hero-btn', {
+        delay: 150,
+        duration: 1000,
+        distance: '60px',
+        easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        origin: 'bottom',
+        interval: 150
+    })
+
+    sr.reveal('.feature-item, .testimonial-item', {
+        duration: 600,
+        distance: '40px',
+        easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        interval: 100,
+    })
+
+    sr.reveal('.service-item--left', {
+        duration: 600,
+        distance: '40px',
+        easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        origin: 'top',
+        interval: 100,
+    })
+
+    sr.reveal('.service-item--right', {
+        duration: 800,
+        distance: '40px',
+        easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        origin: 'right',
+        interval: 100,
+    })
+
+    sr.reveal('.contact', {
+        duration: 500,
+        distance: '40px',
+        easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        origin: 'top',
+        interval: 100,
+    })
+
+    sr.reveal('.contact__title, .contact__description, .contact__form', {
+        delay: 150,
+        duration: 1000,
+        distance: '60px',
+        easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        origin: 'bottom',
+        interval: 150
+    })
+})
