af921157c7844058f2477a6d2daf335a62c27cd5
damazus <liginmlugudor@gmail.com>
2019-07-09 07:54:41 +0000

    contact update2

--- js/app.js
+++ js/app.js
@@ -14,31 +14,21 @@ document.addEventListener('DOMContentLoaded', function () {
         duration: 600,
         distance: '40px',
         easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-        interval: 100,
+        interval: 100
     })
 
     sr.reveal('.service-item--left', {
         duration: 1000,
         distance: '40px',
         easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-        origin: 'top',
-        interval: 100,
+        origin: 'top'
     })
 
     sr.reveal('.service-item--right', {
         duration: 800,
         distance: '40px',
         easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-        origin: 'right',
-        interval: 100,
-    })
-
-    /*sr.reveal('.contact', {
-        duration: 500,
-        distance: '40px',
-        easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-        origin: 'top',
-        interval: 100,
+        origin: 'right'
     })
 
     sr.reveal('.contact__title, .contact__description, .contact__form', {
@@ -48,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
         easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
         origin: 'bottom',
         interval: 150
-    })*/
+    })
 
     // header
     document.querySelector('#header__trigger').addEventListener('click', function (e) {
